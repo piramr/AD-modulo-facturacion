2042894652bb326c6a4af5dd3750469b8aebcacd
alex-r3 <jacoltaa@utn.edu.ec>
2026-07-12 13:07:20 -0500

    fix ssl

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,9 +10,9 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
-    dialectOptions: process.env.NODE_ENV === 'production' ? {
+    dialectOptions: {
       ssl: { require: true, rejectUnauthorized: false }
-    } : {},
+    },
     pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
   }
 );
